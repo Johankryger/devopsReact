069dbc0d41f1224241ae77cf005c012c69d6afcd
JohanKryger <s195491@student.dtu.dk>
2022-09-28 14:24:00 +0200

    removing strict mode

--- web/src/index.js
+++ web/src/index.js
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
